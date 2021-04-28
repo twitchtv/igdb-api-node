9649366a8ed16ab2425927ae8453c16c578d45a6
Jake Cattrall <krazyjakee@gmail.com>
2021-04-28 13:09:57 +0100

    update dependencies

--- test/index.js
+++ test/index.js
@@ -1,38 +1,37 @@
-const assert = require('assert');
-const igdb = require('../lib/index.js').default;
+const assert = require("assert");
+const igdb = require("../lib/index.js").default;
 
 describe("index.js", () => {
-
-  it('Sets auth headers correctly', () => {
+  it("Sets auth headers correctly", () => {
     const client = igdb("my-client-id", "my-app-access-token");
-    assert.equal(client.config.headers["client-id"], "my-client-id");
-    assert.equal(client.config.headers["authorization"], "Bearer my-app-access-token");
+    assert.deepStrictEqual(client.config.headers["client-id"], "my-client-id");
+    assert.deepStrictEqual(client.config.headers["authorization"], "Bearer my-app-access-token");
   });
 
-  it('Throws error when clientID not provided', () => {
-    assert.throws(() => igdb(), Error, "Should throw error when ClientID not provided")
+  it("Throws error when clientID not provided", () => {
+    assert.throws(() => igdb(), Error, "Should throw error when ClientID not provided");
   });
 
-  it('Throws error when appAccessToken not provided', () => {
-    assert.throws(() => igdb("client-id"), Error, "Should throw error when AppAccessToken not provided")
+  it("Throws error when appAccessToken not provided", () => {
+    assert.throws(() => igdb("client-id"), Error, "Should throw error when AppAccessToken not provided");
   });
 
-  it('Should deeply merge options', () => {
+  it("Should deeply merge options", () => {
     const vanilla = igdb("a", "b");
     const normalHeaders = Object.keys(vanilla.config.headers).length;
     const client = igdb("a", "b", {
       headers: {
-        test: 1
-      }
+        test: 1,
+      },
     });
-    assert.equal(Object.keys(client.config.headers).length, normalHeaders + 1);
+    assert.deepStrictEqual(Object.keys(client.config.headers).length, normalHeaders + 1);
   });
 
-  it('Should not convert buffers to json', () => {
+  it("Should not convert buffers to json", () => {
     const client = igdb("a", "b", {
-      responseType: 'arraybuffer',
-      data: Buffer.from("test")
+      responseType: "arraybuffer",
+      data: Buffer.from("test"),
     });
-    assert.equal(client.config.data.constructor, Buffer);
+    assert.deepStrictEqual(client.config.data.constructor, Buffer);
   });
 });
